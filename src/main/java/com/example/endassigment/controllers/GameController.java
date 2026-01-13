540688bff3f17debff52a37d7308353188768bbf
Itzalem <melissa.lj04@gmail.com>
2026-01-13 23:35:02 +0100

    question options

--- src/main/java/com/example/endassigment/controllers/GameController.java
+++ src/main/java/com/example/endassigment/controllers/GameController.java
@@ -1,14 +1,14 @@
 package com.example.endassigment.controllers;
 
-import com.example.endassigment.model.Element;
-import com.example.endassigment.model.GameManager;
-import com.example.endassigment.model.Page;
-import com.example.endassigment.model.Quiz;
+import com.example.endassigment.model.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.VBox;
+
 
 public class GameController {
     @FXML
@@ -19,7 +19,10 @@ public class GameController {
     public VBox questionBox;
     @FXML
     public Label labelTitle;
+    @FXML
+    public VBox answersBox;
 
+    private ToggleGroup toggleGroup;
     private Element currentQuestion;
 
     @FXML
@@ -61,9 +64,41 @@ public class GameController {
 
         labelTitle.setText(question.getTitle());
 
-        //lógica extra
-        //lblScore.setText("Points: " + GameManager.getInstance().getScore());
+        answersBox.getChildren().clear(); //to delete previous answers
+
+        toggleGroup = new ToggleGroup(); //if you select one the other gets deselected
+
+        //radiogorup question
+        if (question instanceof RadiogroupElement) {
+            RadiogroupElement radiogroupQuestion = (RadiogroupElement) question;
+
+            for (String textOption : radiogroupQuestion.getChoices()) {
+                RadioButton button = new RadioButton(textOption);
+                button.setToggleGroup(toggleGroup);
+
+                answersBox.getChildren().add(button);
+            }
+        }
+        //boolean question
+        else if (question instanceof BooleanElement) {
+            BooleanElement boolQuestion = (BooleanElement) question;
+
+            String textTrue = boolQuestion.getLabelTrue();
+            if (textTrue == null) textTrue = "True";
+
+            String textFalse = boolQuestion.getLabelFalse();
+            if (textFalse == null) textFalse = "False";
+
+            RadioButton buttonTrue = new RadioButton(textTrue);
+            buttonTrue.setToggleGroup(toggleGroup);
+
+            answersBox.getChildren().add(buttonTrue);
+
+            RadioButton buttonFalse = new RadioButton(textFalse);
+            buttonFalse.setToggleGroup(toggleGroup);
+
+            answersBox.getChildren().add(buttonFalse);
+        }
     }
-
-
 }
+
