89a7a410ca0839ed279d4cafb50ee20ceb2bed04
Itzalem <melissa.lj04@gmail.com>
2026-01-16 13:03:15 +0100

    EXAM Java on 16/01/26 12.58PM

--- src/main/java/com/example/endassigment/HelloApplication.java
+++ src/main/java/com/example/endassigment/HelloApplication.java
@@ -18,7 +18,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Menu.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("Quizix Application");
         stage.setScene(scene);
 
         stage.setOnCloseRequest(event -> {
